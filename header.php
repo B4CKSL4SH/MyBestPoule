9887d79d77d70420d138542bae65b9938b4587cc
Backslash <fabien.devienne@gmail.com>
2017-04-19 14:57:37 +0200

    add style

--- header.php
+++ header.php
@@ -1,8 +1,15 @@
 <html>
-<head>
-    <title>myBestRoulette</title>
+<header>
+    <div class="max-width">
 
-</head>
+        <div>LOGO</div>
+
+        <div>
+            <a href="create_event.php" title="create_event" class="button">Créer l'évenement</a>
+            <a href="profile.php" title="profile" class="button grey"><i class="fa fa-user-circle" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</header>
 
 <body>
 
