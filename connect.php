bb552eca881f96d1f9da9b6214b4a497bc48f0a6
Arnaud Bonnevigne <arnaud.bonnevigne@wengo.com>
2017-04-19 12:37:06 +0200

    db access

--- connect.php
+++ connect.php
@@ -6,8 +6,12 @@
  * Time: 12:11
  */
 
+
+$user = 'mybestroulette';
+$pass = 'surpriseme';
+
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+    $dbh = new PDO('mysql:host=128.1.1.114;dbname=mybestroulette', $user, $pass);
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
