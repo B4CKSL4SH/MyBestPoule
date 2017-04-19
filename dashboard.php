1d17726e71e1876da49efd6c6578879dc8b01348
Backslash <fabien.devienne@gmail.com>
2017-04-19 14:25:51 +0200

    add file dashboard

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Backslash
+ * Date: 19/04/17
+ * Time: 14:18
+ */
+include_once('functions.php');
+$dbh = get_database();
+
+require('header.php');
+
+?>
+
+<div>
+
+</div>
+
+<?php
+
+require('footer.php');
