f67da0696049a842c718ee874fb634cda39ad7b8
Backslash <fabien.devienne@gmail.com>
2017-04-19 16:42:36 +0200

    integrate profile

--- profile.php
+++ profile.php
@@ -12,21 +12,58 @@
     $userTags = getUserTagsIds();
 ?>
 
-<section class="max-width">
-    <h2>Profil</h2>
-    <div class="create_event">
-        <p class="title" style="margin-top:0;">Vos centres d'intérêts</p>
-        <form id="user_tags_form" action="#" method="POST">
-            <p>
-                <?php foreach (getTags() as $tag) : ?>
-                    <label for="tag_<?php echo $tag["id"] ?>"><?php echo $tag["label"] ?></label>
-                    <input type="checkbox" name="user_tags[]" value="<?php echo $tag["id"] ?>" id="tag_<?php echo $tag["id"] ?>" <?php echo in_array($tag["id"], $userTags) ? 'checked="checked"' : '' ?>/>
-                <?php endforeach; ?>
+    <section class="max-width">
+        <h2>Salutation à toi Symbi'oseur</h2>
+        <p>Dis-nous quels sont tes kiffes dans la vie :)</p>
+        <div class="">
+            <form id="user_tags_form" action="#" method="POST">
+                <div class="row" >
+                    <div class="column event">
+                        <i class="fa fa-ticket" aria-hidden="true" style="background: #e9256a;"></i>
+                        <label for="tag_5">Cinema</label>
+                        <input type="checkbox" name="user_tags[]" value="5" id="tag_5" />
+                    </div>
+                    <div class="column event">
+                        <i class="fa fa-music" aria-hidden="true" style=""></i>
+                        <label for="tag_7">Concert</label>
+                        <input type="checkbox" name="user_tags[]" value="7" id="tag_7" />
+                    </div>
+                    <div class="column event violet">
+                        <i class="fa fa-picture-o" aria-hidden="true" style=""></i>
+                        <label for="tag_6">Culture</label>
+                        <input type="checkbox" name="user_tags[]" value="6" id="tag_6" checked="checked"/>
+                    </div>
+                    <div class="column event gold">
+                        <i class="fa fa-trophy" aria-hidden="true" style=""></i>
+                        <label for="tag_1">Escape Game</label>
+                        <input type="checkbox" name="user_tags[]" value="1" id="tag_1" />
+                    </div>
+
+                    <div class="column event">
+                        <i class="fa fa-gamepad" aria-hidden="true" style="background: #26bea7;"></i>
+                        <label for="tag_2">Loisirs</label>
+                        <input type="checkbox" name="user_tags[]" value="2" id="tag_2" />
+                    </div>
+
+                    <div class="column event">
+                        <i class="fa fa-futbol-o" aria-hidden="true" style="background:  #f7c600;"></i>
+
+                        <label for="tag_3">Sport</label>
+                        <input type="checkbox" name="user_tags[]" value="3" id="tag_3" checked="checked"/>
+                    </div>
 
                     <br />
-                <input type="submit" id="user_tags_form_submit" class="button" style="border: none; color: #ff4e00; margin-top: 30px;"/>
-            </p>
-        </form>
-    </div>
-</section>
+                </div>
+                <h2 style="margin-bottom:0;">et aussi...</h2>
+                <center class="column event" style="margin:auto;width:20%;">
+                    <i class="fa fa-question" aria-hidden="true" style="background:#fff;color:#e64215;"></i>
+                    <label for="tag_3">Surprenez-moi !</label>
+                    <input type="checkbox" name="user_tags[]" value="3" id="tag_" checked=""/>
+                </center>
+
+                <center style="width:70%;margin:20px auto 0;"><input type="submit" id="user_tags_form_submit" class="button" style="border: none; color: #ff4e00; margin-top: 30px;" value="Allons-y !"/></center>
+
+            </form>
+        </div>
+    </section>
 <?php include_once 'footer.php'; ?>
