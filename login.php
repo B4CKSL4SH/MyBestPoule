aabb04e59e2c8dca1b56fd2f6d07ccfa591329a7
Backslash <fabien.devienne@gmail.com>
2017-04-19 14:15:33 +0200

    fix login

--- login.php
+++ login.php
@@ -1,36 +1,40 @@
-<html>
-    <head>
-    </head>
-    <body>
-        <div>
-            <label for="login">Portail de connection</label>
-            <form name="login" method="post">
-                <label for="email">Votre Email</label>
-                <input type="email" name="email" id="email" placeholder="Votre Email" required /><br />
-                <label for="password">Votre mot de passe</label>
-                <input type="password" name="password" id="password" placeholder="Votre mot de passe" required /><br />
-                <input type="submit" name="login" value="Se connecter" />
-            </form>
+<?php
+include_once('functions.php');
+$dbh = get_database();
 
-            <label for="signup">Portail d'inscription</label>
-            <form name="signup" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                <label for="email">Votre Email</label>
-                <input type="email" name="email" id="email" placeholder="Votre Email" required /><br />
-                <label for="password">Votre mot de passe</label>
-                <input type="password" name="password" id="password" placeholder="Votre mot de passe" required /><br />
-                <input type="submit" name="signup" value="S'inscrire" />
-            </form>
-        </div>
-        <?php
-        if (!empty($_POST) && isset($_POST['login'])) {
-            // connection du user + redirection vers le dashboard
-            // $login = $_POST['email'];
-            // $pswd = $_POST['password'];
-        } elseif (!empty($_POST) && isset($_POST['signup'])) {
-            // creation du user + redirection vers choix des filtres
-            // $login = $_POST['email'];
-            // $pswd = $_POST['password'];
-        }
-        ?>
-    </body>
-</html>
+require('header.php');
+    if (!empty($_POST) && isset($_POST['login'])) {
+        // connection du user + redirection vers le dashboard
+        // $login = $_POST['email'];
+        // $pswd = $_POST['password'];
+    } elseif (!empty($_POST) && isset($_POST['signup'])) {
+        // creation du user + redirection vers choix des filtres
+        // $login = $_POST['email'];
+        // $pswd = $_POST['password'];
+    }
+?>
+
+<div>
+    <label for="login">Portail de connection</label>
+    <form name="login" method="post">
+        <label for="email">Votre Email</label>
+        <input type="email" name="email" id="email" placeholder="Votre Email" required /><br />
+        <label for="password">Votre mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="Votre mot de passe" required /><br />
+        <input type="submit" name="login" value="Se connecter" />
+    </form>
+
+    <label for="signup">Portail d'inscription</label>
+    <form name="signup" method="post">
+        <label for="email">Votre Email</label>
+        <input type="email" name="email" id="email" placeholder="Votre Email" required /><br />
+        <label for="password">Votre mot de passe</label>
+        <input type="password" name="password" id="password" placeholder="Votre mot de passe" required /><br />
+        <input type="submit" name="signup" value="S'inscrire" />
+    </form>
+</div>
+
+
+<?php
+
+require('footer.php');
